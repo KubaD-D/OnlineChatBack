0583b239bfe12a56f31375e3de36fd4387003533
KubaDD <kuburolek11@gmail.com>
2024-02-09 00:52:57 +0100

    Add project files.

---
+++ OnlineChatBack/Models/ChatRoom.cs
@@ -0,0 +1,18 @@
+﻿namespace OnlineChatBack.Models
+{
+    public class ChatRoom
+    {
+        public Guid Id { get; set; }
+        public List<User> Users { get; set; } = new();
+
+        public void AddUser(User user)
+        {
+            Users.Add(user);
+        }
+
+        public bool ContainsUser(User user)
+        {
+            return Users.Contains(user);
+        }
+    }
+}
